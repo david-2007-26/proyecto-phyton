b5f70a37f92a593a9bb360b11b8937adc77e15dc
David-2007-26 <santimohe3@gmail.com>
2026-08-26 10:29:38 -0500

    mi p´rimer cmmit

---
+++ main.py
@@ -0,0 +1,46 @@
+# menu
+
+while True:
+    print("=========================")
+    print("registro de entrenamiento")
+    print("=========================")
+    print("1. ejercicio del dia")
+    print("2. ver historial semanal")
+    print("3. cacular progreso")
+    print("4. buscar ejercicio")
+    print("5. salir")
+
+
+    opcion = input("seleccione una opcion ")
+
+
+    if opcion == "1":
+        print("ejercicio del dia") 
+        dia = input
+
+
+
+
+
+
+
+
+
+    elif opcion == "2":
+        print("ver historial semanal")      
+
+    elif opcion == "3":
+        print("calcular pogreso") 
+
+    elif opcion == "4":
+        print("buscar ejercicio") 
+
+    elif opcion == "5":
+        print("salir")   
+        break        
+
+    else:
+        print("opcion no valida")    
+print('Gracias por utilizar el sistema')     
+
+
